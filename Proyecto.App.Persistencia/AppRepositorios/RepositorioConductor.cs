35b64b06aa00a0c7f82036c2c5d0f0262c3a8d6c
Edwin J. Medina <edwin.jmg@gmail.com>
2022-09-12 21:54:16 -0500

    Cambios en la barra de navegación del FrontEnd

--- Proyecto.App.Persistencia/AppRepositorios/RepositorioConductor.cs
+++ Proyecto.App.Persistencia/AppRepositorios/RepositorioConductor.cs
@@ -8,6 +8,45 @@ namespace Proyecto.App.Persistencia.AppRepositorios
 {
     public class RepositorioConductor : IRepositorioConductor
     {
-
+        public readonly AppContext _appContext;
+        public RepositorioConductor(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+        Conductor IRepositorioConductor.AgregarConductor(Conductor Conductor)
+        {
+            var ConductorNuevo = _appContext.Conductores.Add(Conductor);
+            _appContext.SaveChanges();
+            return ConductorNuevo.Entity;
+        }
+        void IRepositorioConductor.EliminarConductor(int idConductor)
+        {
+            var ConductorEncontrado = _appContext.Conductores.FirstOrDefault(u=>u.Id==idConductor);
+            if(ConductorEncontrado==null)
+            return;
+            _appContext.Conductores.Remove(ConductorEncontrado);
+            _appContext.SaveChanges();
+        }
+        IEnumerable<Conductor> IRepositorioConductor.MostrarConductores()
+        {
+            return _appContext.Conductores;
+        }
+        Conductor IRepositorioConductor.MostrarConductor(int idConductor)
+        {
+            return _appContext.Conductores.FirstOrDefault(c=>c.Id==idConductor);
+        }
+        Conductor IRepositorioConductor.ActualizarConductor(Conductor Conductor)
+        {
+            var ConductorEncontrado = _appContext.Conductores.FirstOrDefault(c=>c.Id==Conductor.Id);
+            if(ConductorEncontrado!=null)
+            {
+                ConductorEncontrado.Cedula=Conductor.Cedula;
+                ConductorEncontrado.Nombre=Conductor.Nombre;
+                ConductorEncontrado.Apellido=Conductor.Apellido;
+                ConductorEncontrado.Celular=Conductor.Celular;
+                _appContext.SaveChanges();
+            }
+            return ConductorEncontrado;
+        }
     }
 }
